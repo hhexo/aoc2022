5b9d0c092a58d01f21796fa43573429c939cd6ba
Dario Domizioli <dario.domizioli@gmail.com>
2022-12-22 12:18:24 +0100

    Reworked p04

--- src/bin/p04.rs
+++ src/bin/p04.rs
@@ -14,36 +14,39 @@
 
 use aoc2022::{AoCError, InputText};
 
-use std::collections::HashSet;
+fn aabb_intersect(a1: i32, a2: i32, b1: i32, b2: i32) -> bool {
+    return a1 >= b1 && a1 <= b2 ||
+           b1 >= a1 && b1 <= a2
+}
+
+fn aabb_contained(a1: i32, a2: i32, b1: i32, b2: i32) -> bool {
+    return a1 >= b1 && a1 <= b2 && a2 >= b1 && a2 <= b2 ||
+           b1 >= a1 && b1 <= a2 && b2 >= a1 && b2 <= a2
+}
 
 fn main() -> Result<(), AoCError> {
     let input = InputText::new(4)?;
 
-    let pairs_of_sets = input.lines()?
+    // Read lines to a vector where each element is a vector of 4 numbers:
+    // a1 a2 b1 b2
+    let four_number_vectors = input.lines()?
     .map(|line| line.unwrap_or_default())
     .map(|actual_line| {
-        actual_line.split(",").map(|pair| {
+        actual_line.split(",").fold(Vec::new(), |mut acc, pair| {
             let nums: Vec<i32> = pair.split("-").map(|num| i32::from_str_radix(num, 10).unwrap_or(-1)).collect();
             if nums.len() >= 2 {
-                if nums[1] >= nums[0] {
-                    (nums[0]..nums[1]+1).collect::<HashSet<i32>>()
-                } else {
-                    HashSet::new()
-                }
-            } else {
-                HashSet::new()
+                acc.push(nums[0]);
+                acc.push(nums[1]);
             }
-        }).collect::<Vec<HashSet<i32>>>()
-    }).collect::<Vec<Vec<HashSet<i32>>>>();
-
-    let part1 = pairs_of_sets.iter().fold(0, |acc, sets| {
-        acc + i32::from(sets.len() >= 2 && {
-            let intersection_size = sets[0].intersection(&sets[1]).count();
-            intersection_size == sets[0].len() || intersection_size == sets[1].len()
+            acc
         })
+    }).collect::<Vec<Vec<i32>>>();
+
+    let part1 = four_number_vectors.iter().fold(0, |acc, tuple| {
+        acc + i32::from(tuple.len() == 4 && aabb_contained(tuple[0], tuple[1], tuple[2], tuple[3]))
     });
-    let part2 = pairs_of_sets.iter().fold(0, |acc, sets| {
-        acc + i32::from(sets.len() >= 2 && sets[0].intersection(&sets[1]).next().is_some())
+    let part2 = four_number_vectors.iter().fold(0, |acc, tuple| {
+        acc + i32::from(tuple.len() == 4 && aabb_intersect(tuple[0], tuple[1], tuple[2], tuple[3]))
     });
 
     println!("Part 1: {}", part1);
